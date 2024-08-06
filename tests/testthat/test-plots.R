3c02b21b3c6eb3acc553256bdb407ac8477585e8
Anna Bortolato <anna.bortolato@unipd.it>
2024-08-06 17:05:31 +0200

    example of test for plots

---
+++ tests/testthat/test-plots.R
@@ -0,0 +1,22 @@
+env <- test_env()
+graph <- readRDS(test_path("fixtures", "graph.rds"))
+mo <- fake_mo(genes=nodes(graph), type="survival")
+res <- multiOmicsSurvivalModuleTest(mo, graph)
+assign("graph", graph, envir = env)
+assign("mo", mo, envir = env)
+assign("res", res, envir = env)
+
+
+test_that("multiOmicsSurvialTest", {
+  res <- multiOmicsSurvivalModuleTest(mo, graph)
+  expect_s4_class(res, "MultiOmicsModules")
+  p <- plotModuleKM(res, 1, formula="Surv(days, status) ~ expPC1", envir=env)
+  expect_s3_class(p, "ggsurvplot")
+  expect_false(is.null(p))
+})
+
+
+
+
+
+
