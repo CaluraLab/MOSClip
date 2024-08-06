3760559a9ab1b1a0299b12c073531569af85f08a
Anna Bortolato <anna.bortolato@unipd.it>
2024-08-06 17:23:59 +0200

    new test for makeOmics function

---
+++ tests/testthat/test-makeOmics.R
@@ -0,0 +1,34 @@
+test_that("makeOmics", {
+  fake_exp <- dummy_expression_like_dataset()
+  fake_exp2 <- fake_exp[,-1]
+  new_order <- sample(colnames(fake_exp))
+  fake_ord <- fake_exp[, new_order]
+  new_order_dup <- sample(row.names(fake_exp), replace = TRUE)
+  fake_dup <- fake_exp[new_order_dup,]
+  
+  expect_message(makeOmics(experiments = ExperimentList(exp=fake_exp)), 
+                 "Data and relative methods to analyze them must be equal in length.")
+  expect_message(makeOmics(experiments = ExperimentList(exp=fake_exp, exp2=fake_exp),
+                           modelInfo = c("summarizeWithPca", "test"),
+                           specificArgs = "test"),
+                 "data and specificArgs must be equal in length.")
+  expect_message(makeOmics(experiments = ExperimentList(exp=fake_exp),
+                           modelInfo = "test",
+                           specificArgs = "test"),
+                 "test modelInfo not found in method. Try availableOmicMethods.")
+  expect_message(makeOmics(experiments = ExperimentList(exp=fake_exp, exp2=fake_exp2),
+                           modelInfo = c("summarizeWithPca", "summarizeWithPca"),
+                           specificArgs = list("pca", "pca")),
+                 "Mismatch in sample numbers")
+  expect_message(makeOmics(experiments = ExperimentList(exp=fake_exp, exp2=fake_dup),
+                           modelInfo = c("summarizeWithPca", "summarizeWithPca"),
+                           specificArgs = list("pca", "pca")),
+                 "Duplicated row.names found in omics ")
+  expect_message(mo <- makeOmics(experiments = ExperimentList(exp=fake_exp, exp2=fake_ord),
+                                 modelInfo = c("summarizeWithPca", "summarizeWithPca"),
+                                 specificArgs = list("pca", "pca")),
+                 "Samples order mismatch")
+  expect_null(mo)
+  expect_s4_class(fake_mo(), "Omics")
+})
+
