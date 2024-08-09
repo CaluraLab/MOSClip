c06754ea2f54bab8c1f9378369819a592c5c14a8
Anna Tanada <annat@medium09.romualdi.bio.unipd.it>
2024-08-09 12:48:31 +0200

    tests for all plots

---
+++ tests/testthat/test-PlottingMO.R
@@ -0,0 +1,138 @@
+test_that("Plots with two class object work", {
+  # does not work w get()
+  dummy_react <- readRDS(test_path("fixtures", "reactSmallDummy.rds"))
+  dummy_omics <- readRDS(test_path("fixtures", "dummyOmics.rds"))
+  dummy_annot <- readRDS(test_path("fixtures", "dummyAnnot.rds"))
+  
+  twoCPathwayObj <- multiOmicsTwoClassesPathwayTest(dummy_omics,
+                                                    dummy_react[[1]],
+                                                    dummy_annot,
+                                                    baseFormula = "classes ~",
+                                                    nullModel = "classes ~ 1")
+  res.pathwayHeat <- plotPathwayHeat(
+    twoCPathwayObj, dummy_omics, sortBy = c("expPC2", "mut", "classes"),
+    paletteNames = c(exp = "red", met = "green", mut = "blue", cnv = "yellow"),
+    additionalAnnotations = dummy_annot,
+    additionalPaletteNames = list(classes = "teal"),
+    nrowsHeatmaps = 2, withSampleNames = F)
+  
+  expect_false(is.null(res.pathwayHeat))
+  expect_identical(class(res.pathwayHeat), c("gg", "ggplot"))
+  
+  expect_error(plotPathwayHeat(
+    twoCPathwayObj, dummy_omics, sortBy = c("expPC2", "mut", "classes"),
+    paletteNames = c(exp = "red", met = "green"),
+    additionalAnnotations = dummy_annot,
+    additionalPaletteNames = list(classes = "teal"),
+    nrowsHeatmaps = 2, withSampleNames = F), "omic not found")
+  
+  res.pathwayReport <- plotMultiPathwayReport(list(twoCPathwayObj),
+    MOcolors = c(exp = "red", met = "green", mut = "blue"),
+    top = 40, fontsize = 10)
+  
+  expect_false(is.null(res.pathwayReport))
+  expect_s4_class(res.pathwayReport, "HeatmapList")
+  
+  expect_error(
+    plotMultiPathwayReport(
+      list(twoCPathwayObj),
+      MOcolors = c(exp = "red", met = "green", mut = "blue", cnv = "yellow"),
+      top = 40, fontsize = 10), "Length of MOcolors differs")
+  
+  twoCModuleObj <- multiOmicsTwoClassesModuleTest(dummy_omics,
+                                                  dummy_react[[1]],
+                                                  dummy_annot,
+                                                  baseFormula = "classes ~",
+                                                  nullModel = "classes ~ 1")
+  res.moduleHeat <- plotModuleHeat(
+    twoCModuleObj, dummy_omics, 2, paletteNames = c(
+      exp = "red", met = "green", mut = "blue", cnv = "yellow"),
+    additionalAnnotations = dummy_annot,
+    additionalPaletteNames = list(classes = "teal"),
+    sortBy = c("met2k", "expPC1", "classes"), withSampleNames = F)
+  
+  expect_false(is.null(res.moduleHeat))
+  expect_identical(class(res.moduleHeat), c("gTree", "grob", "gDesc"))
+  
+  expect_error(plotModuleHeat(
+    twoCModuleObj, dummy_omics, 2, sortBy = c("expPC2", "mut", "classes"),
+    paletteNames = c(exp = "red", met = "green"),
+    additionalAnnotations = dummy_annot,
+    additionalPaletteNames = list(classes = "teal"),
+    nrowsHeatmaps = 2, withSampleNames = F), "omic not found")
+  
+  res.ModuleInGraph <- plotModuleInGraph(
+    twoCModuleObj, dummy_omics, dummy_react,
+    moduleNumber = 3, legendLabels = c("expr", "methylation", "cnv"),
+    paletteNames = c(exp = "red", met = "green", mut = "blue", cnv = "yellow"))
+  
+  expect_false(is.null(res.ModuleInGraph))
+  expect_true(is.list(res.ModuleInGraph))
+  expect_true(all(vapply(res.ModuleInGraph, function(x) !is.null(x),
+                         logical(1))))
+  
+  expect_error(plotModuleInGraph(
+    twoCModuleObj, dummy_omics, dummy_react,
+    moduleNumber = 3, legendLabels = c("expr", "methylation", "cnv"),
+    paletteNames = c(
+      exp = "orange", met = "green", mut = "blue", cnv = "yellow")),
+    "paletteNames value is not allowed")
+  expect_error(plotModuleInGraph(
+    twoCModuleObj, dummy_omics, dummy_react,
+    moduleNumber = 3, legendLabels = c("expr", "methylation", "cnv"),
+    paletteNames = c(exp = "red")), "Missing palette")
+  
+  res.moduleReport <- plotModuleReport(
+    twoCModuleObj, MOcolors = c( exp = "red", met = "green", mut = "blue"))
+  expect_false(is.null(res.moduleReport))
+  expect_s4_class(res.moduleReport, "HeatmapList")
+  expect_error(
+    plotModuleReport(
+      twoCModuleObj,
+      MOcolors = c(exp = "red", met = "green", mut = "blue", cnv = "yellow")),
+    "Length of MOcolors differs")
+})
+
+test_that("Kaplan Meier plots work", {
+  dummy_react <- readRDS(test_path("fixtures", "reactSmallDummy.rds"))
+  dummy_omicsSurv <- createFakeOmics("survival")
+  
+  res.survPathway <- multiOmicsSurvivalPathwayTest(
+    dummy_omicsSurv, dummy_react[[1]], survFormula = "Surv(days, status) ~",
+    autoCompleteFormula = T)
+  
+  res.PathwayKM <- plotPathwayKM(
+    res.survPathway, dummy_omicsSurv,
+    formula = "Surv(days, status) ~ met3k + expPC2",
+    paletteNames = "Paired")
+  
+  expect_false(is.null(res.PathwayKM))
+  expect_identical(class(res.PathwayKM), c("ggsurvplot", "ggsurv", "list"))
+  
+  res.survModules <- multiOmicsSurvivalModuleTest(
+    dummy_omicsSurv, dummy_react[[1]], survFormula = "Surv(days, status) ~",
+    autoCompleteFormula = TRUE)
+  
+  res.ModuleKM <- plotModuleKM(
+    res.survModules, dummy_omicsSurv, 2,
+    formula = "Surv(days, status) ~ met2k + expPC1",
+    paletteNames = "Paired", risk.table = TRUE, size = 2, inYears = TRUE)
+  
+  expect_false(is.null(res.ModuleKM))
+  expect_identical(class(res.ModuleKM), c("ggsurvplot", "ggsurv", "list"))
+  
+  expect_error(plotModuleKM(
+    res.survModules, dummy_omicsSurv, 2,
+    formula = "Surv(days, status) ~ met2k + expPC1",
+    paletteNames = "Paired", risk.table = TRUE, size = 2, inYears = TRUE,
+    additional_discrete = "recurrence_status"),
+    "discrete variables were not found")
+  
+  expect_error(plotModuleKM(
+    res.survModules, dummy_omicsSurv, 2,
+    formula = "Surv(days, status) ~ met2k + expPC1",
+    paletteNames = "Paired", risk.table = TRUE, size = 2, inYears = TRUE,
+    additional_continuous = "recurrence_days"),
+    "continuous variables were not found")
+  
+})
