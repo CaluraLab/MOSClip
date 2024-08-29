12e7499c78ecffabe6b47dac5464acb002006db5
Anna Tanada <annat@medium09.romualdi.bio.unipd.it>
2024-08-29 14:24:46 +0200

    changes to make it work with superexactest and supertest

--- tests/testthat/fixtures/make-reactome-obj.R
+++ tests/testthat/fixtures/make-reactome-obj.R
@@ -1,9 +1,24 @@
 # Creating reactome object for test files
+load("inst/TCGA-OV-hg38-preprocessed-externalSurv.RData")
+expression <- rnaSeq
+row.names(expression) <- paste0("ENTREZID:", row.names(expression))
 
-react_dummy <- graphite::pathways("hsapiens", "reactome")
-react_dummy <- graphite::convertIdentifiers(reactome, "entrez")
-nodesLength <- sapply(reactome, function(g) {
-  length(intersect(graphite::nodes(g), row.names(pseudoExpNorm)))})
-reactSmallDummy <- reactome[nodesLength >= 20 & nodesLength <= 100]
+if (file.exists("reactome-entrez.RData")) {
+  whole_reactome <- readRDS(test_path("fixtures", "reactome-entrez"))
+} else {
+  reactome <- pathways("hsapiens", "reactome")
+  reactome <- convertIdentifiers(reactome, "entrez")
+  saveRDS(reactome, file = "test/testthat/fixtures/reactome-entrez.rds")
+}
+
+# Creating smaller version for smaller tests
+nodesLength <- sapply(react_dummy, function(g) {
+  length(intersect(graphite::nodes(g), row.names(expression)))})
+reactSmallDummy <- react_dummy[nodesLength >= 20 & nodesLength <= 100]
 
 saveRDS(reactSmallDummy[1], file = "reactSmallDummy.rds")
+
+# Saving reactome object with 5 pathways for frequencies plot
+saveRDS(reactSmallDummy[1:5],
+        file = "tests/testthat/fixtures/reactBiggerDummy.rds")
+
