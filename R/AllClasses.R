c6193b20246b563b7e50b16bd8b09af30679f491
Anna Bortolato <anna.bortolato@unipd.it>
2024-09-02 15:19:27 +0200

    adjust documentation and line width

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -3,38 +3,42 @@ setClassUnion("characterOrNULL", c("character", "NULL"))
 
 #' Multi Omics Modules.
 #'
-#' This class organize the results of the Multi Omics Module Survival Test analysis.
+#' This class organizes the results of the Multi-Omics Module Survival Test 
+#' analysis.
 #'
-#' @slot alphas numeric vector of the pvalues of all the modules.
-#' @slot zlists a list of all the zs of the covariates.
-#' @slot coxObjs a list with all the data.frame used for coxph of each module.
-#' @slot modulesView a list of module information: for each omic the module data, the method used and the covariate analyzed.
-#' @slot modules a list woth the genes that belong to the module.
-#' @slot formulas a list, for each module the character of the formula used in the coxph.
+#' @slot alphas a numeric vector of the pvalues of all the modules.
+#' @slot zlists a list of numeric vectors with the zs of the covariates 
+#' for each module.
+#' @slot modulesView a list of module information: for each omic, 
+#' the name of the omic, the genes used, the method, the name of the 
+#' covariates analyzed and other specific information based on the omic.
+#' @slot modules a list with the genes that belong to the module.
 #' @slot title the name of the pathway.
-#' @slot analysis The type of analysis done: survival or two-class.
+#' @slot analysis the type of analysis done: survival or two-class.
 #' @name MultiOmicsModules-class
 #' @rdname MultiOmicsModules-class
 #'
 setClass(Class ="MultiOmicsModules",
-         slots = c(alphas  = "numeric",
-                   zlists  = "list",
-                   modulesView  = "list",
-                   modules     = "list",
+         slots = c(alphas = "numeric",
+                   zlists = "list",
+                   modulesView = "list",
+                   modules = "list",
                    analysis = "characterOrNULL",
                    multiOmicObj = "characterOrNULL",
                    title = "characterOrNULL"))
 
 #' Multi Omics Pathway.
 #'
-#' This class organize the results of the Multi Omics Module Survival Test analysis.
+#' This class organize the results of the Multi-Omics Pathway Survival Test 
+#' analysis.
 #'
-#' @slot pvalue numeric, the pvalues of the whole module.
-#' @slot zlist a vector of all the zs of the covariates.
-#' @slot coxObj a data.frame used for the coxph model.
-#' @slot pathView a list, for each omic the pathway data, the method used and the covariate analyzed.
+#' @slot pvalue a numeric vector of the pvalues of the pathways.
+#' @slot zlist a numeric vector with the zs of all the covariates. 
+#' @slot pathView a list of pathway information: for each omic, 
+#' the name of the omic, the genes used, the method, the name of the 
+#' covariates analyzed and other specific information based on the omic.
 #' @slot title the name of the pathway.
-#' @slot analysis The type of analysis done: survival or two-class.
+#' @slot analysis the type of analysis done: survival or two-class.
 #' @name MultiOmicsPathway-class
 #' @rdname MultiOmicsPathway-class
 #'
@@ -46,16 +50,16 @@ setClass(Class ="MultiOmicsPathway",
                    multiOmicObj = "characterOrNULL",
                    title = "characterOrNULL"))
 
-# setClass(Class ="SinglePath", package = "biocmosclip",
-#          slots = c(global = "MultiOmicsPathway",
-#                    modules = "MultiOmicsModules"))
 
 #' Omics.
 #' 
-#' This class is the storage for the different omic datasets that we need to analyze.
+#' This class is the storage for the different omic datasets that we need to 
+#' analyze. It is based on `MultiAssayExperiment`.
 #'
-#' @slot modelInfo a list with length equal to length(data) that are modelInfo to process each dataset.
-#' @slot specificArgs a list with length equal to length(data) to set additional parameters specific of the modelInfo.
+#' @slot modelInfo a list with length equal to length(data) that are modelInfo 
+#' to process each dataset.
+#' @slot specificArgs a list with length equal to length(data) to set additional 
+#' parameters specific of the modelInfo.
 #'
 #' @name Omics-class
 #' @rdname Omics-class
