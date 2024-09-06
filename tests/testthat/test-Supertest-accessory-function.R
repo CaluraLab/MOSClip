13ed96dfb72d2d6439f43cc857f04ae284596fe5
Anna Bortolato <anna.bortolato@unipd.it>
2024-09-06 15:34:56 +0200

    update tests

--- tests/testthat/test-Supertest-accessory-function.R
+++ tests/testthat/test-Supertest-accessory-function.R
@@ -1,16 +1,13 @@
-
-library(graphite)
-pathHierarchy <- downloadPathwayRelationFromReactome()
-pathHierarchyGraph <- igraph::graph_from_data_frame(
-  d = pathHierarchy, directed = TRUE)
-
-
-pathwayName = c("Circadian Clock", "Signaling Pathways")
-reactome <- pathways("hsapiens", "reactome")[pathwayName]
-
-
-omicsClasses2pathways <- list(exp=pathwayName, 'cnv;exp'="Not a pathway")
 test_that("generateWarn", {
+  pathHierarchy <- downloadPathwayRelationFromReactome()
+  pathHierarchyGraph <- igraph::graph_from_data_frame(
+    d = pathHierarchy, directed = TRUE)
+  
+  pathwayName = c("Circadian Clock", "Signaling Pathways")
+  reactome <- graphite::pathways("hsapiens", "reactome")[pathwayName]
+  
+  
+  omicsClasses2pathways <- list(exp=pathwayName, 'cnv;exp'="Not a pathway")
   expect_warning(omicsClasses2fathers <- lapply(omicsClasses2pathways,
                                                 annotePathwayToFather, 
                                                 graphiteDB=reactome,
